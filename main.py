509b4af1acb6e6889cf0bb5f896240550b6f9820
Moral-artist <569202588@qq.com>
2026-09-23 17:10:36 +0700

    Add the access for the frontend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from Router.auth.auth import router as auth_router
 from Router.user.user import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="Training ERP",
     description="企业培训考试管理系统",
@@ -15,3 +16,16 @@ def home():
 
 app.include_router(auth_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
